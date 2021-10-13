e20652c2b6d3043a10a5acd58a9c38d6c9edd7ca
Yuzhe <yuzhe.hao.cn@gmail.com>
2021-10-14 01:46:10 +0900

    added fade-in-out function

--- js/scripts.js
+++ js/scripts.js
@@ -20,42 +20,7 @@ function update_timestamp() {
         if (minute < 10) { minute = '0' + String(minute) };
         if (second < 10) { second = '0' + String(second) };
 
-        var local = `${month}/${day}/${year} ${hour}:${minute}${period} TOKYO`;
+        var local = `${month}/${day}/${year} ${hour}:${minute}${period} TYO`;
         $('time')[0].innerHTML = local
     });
-}
-
-var fade_in_from = 0;
-var fade_out_from = 10;
-
-function fadeIn(element) {
-    var target = document.getElementById(element);
-    target.style.display = "block";
-    var newSetting = fade_in_from / 10;
-    target.style.opacity = newSetting;
-    // opacity ranges from 0 to 1
-    fade_in_from++;
-    if (fade_in_from == 10) {
-        target.style.opacity = 1;
-        clearTimeout(loopTimer);
-        fade_in_from = 0;
-        return false;
-    }
-    var loopTimer = setTimeout('fadeIn(\'' + element + '\')', 100);
-}
-
-function fadeOut(element) {
-    var target = document.getElementById(element);
-    var newSetting = fade_out_from / 10;
-    target.style.opacity = newSetting;
-    fade_out_from--;
-    if (fade_out_from == 0) {
-        target.style.opacity = 0;
-        target.style.display = "none";
-        clearTimeout(loopTimer);
-        fade_out_from = 10;
-        return false;
-    }
-    var loopTimer = setTimeout('fadeOut(\'' + element + '\')', 100);
-    window.location.href = "link.html";
 }
