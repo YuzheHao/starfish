0391f4604118ee99afe49c6155aa8c03fd67df8a
Yuzhe <yuzhe.hao.cn@gmail.com>
2021-10-13 19:19:14 +0900

    modified js month

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@ function update_timestamp() {
     $(document).ready(function() {
         now = new Date()
         var year = now.getFullYear();
-        var month = now.getMonth();
+        var month = now.getMonth() + 1;
         var day = now.getDate();
         var hour = now.getHours();
         var minute = now.getMinutes();
